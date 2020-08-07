21c4d25c5dbc08a59769df8e50824547deefbccc
PunkerIsaacs <punkerisaacs@hotmail.com>
2020-08-06 22:22:45 -0500

    initial app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import { HomeView } from './views';
+import { Header } from './components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(): JSX.Element {
+    return (
+        <div className="container-fluid app">
+            <Header />
+            <Switch>
+                <Route path="/" component={HomeView} exact />
+            </Switch>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/views/home/HomeView.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class HomeView extends Component {
+    render(): JSX.Element {
+        return <div>home view</div>;
+    }
+}
